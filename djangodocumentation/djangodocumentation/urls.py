51f5d6aa6a50d8eb834ecbca7a8088a449572731
adeladanseun <oluwaseunadeladan94@gmail.com>
2024-09-10 09:49:30 +0000

    modified:   djangodocumentation/djangodocumentation/settings.py 	modified:   djangodocumentation/djangodocumentation/urls.py 	new file:   djangodocumentation/newapp/urls.py 	modified:   djangodocumentation/newapp/views.py

--- djangodocumentation/djangodocumentation/urls.py
+++ djangodocumentation/djangodocumentation/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from newapp import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("newapp/", include('newapp'))
 ]
